bc53bf57bcd4d7e9567f575a99a902fa56c98ba2
Marshall Sharp <ms279226@falmouth.ac.uk>
2023-12-31 19:14:53 +0000

    init github

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    Input playerControls;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControls = GetComponent<Input>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
